bc15932fbe880a315bfff56800cbeb58f81fcdef
Gaurav <gauravsinghbora445@gmail.com>
2025-12-10 11:30:40 +0530

    docker

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,17 +1,21 @@
-def call(String projectname, String tagname,String dockerusername){
-   echo 'Push Code'
-   withCredentials([
-                    usernamePassword(
-                        credentialsId: 'docker-hub-cred',
-                        usernameVariable: 'dockerhubusername',
-                        passwordVariable: 'dockerhubpassword'
-                    )
-                ]) {
-                    sh "docker login -u ${dockerhubusername} -p ${dockerhubpassword}"
-                    sh "docker image tag ${projectname}:${taganme} ${dockerusername}/
-                    ${projectname}:${taganme}"
-                    sh "docker push ${dockerusername}/
-    ${projectname}:${taganme}"
-                }
-    echo"pushed code"
+def call(String projectname, String tagname, String dockerusername) {
+
+    echo 'Push Code'
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'docker-hub-cred',
+            usernameVariable: 'dockerhubusername',
+            passwordVariable: 'dockerhubpassword'
+        )
+    ]) {
+
+        sh "docker login -u ${dockerhubusername} -p ${dockerhubpassword}"
+
+        sh "docker image tag ${projectname}:${tagname} ${dockerusername}/${projectname}:${tagname}"
+
+        sh "docker push ${dockerusername}/${projectname}:${tagname}"
+    }
+
+    echo "Pushed code"
 }
